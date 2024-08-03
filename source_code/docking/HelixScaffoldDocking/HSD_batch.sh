eabec20b0e10de16bc7279d46bcab266ed47ec56
Laiyi Feng <88279459+laiyii@users.noreply.github.com>
2024-08-03 14:30:28 +0800

    Update HSD_batch.sh

--- source_code/docking/HelixScaffoldDocking/HSD_batch.sh
+++ source_code/docking/HelixScaffoldDocking/HSD_batch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# 解析命令行参数
 while getopts "t:b:a:" opt; do
   case $opt in
     t) target="$OPTARG"
@@ -14,11 +13,9 @@ while getopts "t:b:a:" opt; do
   esac
 done
 
-# 检查是否所有参数都已提供
 if [ -z "$target" ] || [ -z "$batch_info" ] || [ -z "$atomid" ]; then
   echo "Usage: $0 -t target -b batch_info -a atomid"
   exit 1
 fi
 
-# 执行命令
 $HSD/docking/HelixScaffoldDocking/HelixScaffoldDocking_batch "$target" "$batch_info" "$atomid"
